045e6c6d392103e43c36f12bae408af7ef5e7d63
Richard Wackerbarth <rkw@dataplex.net>
2018-12-24 13:01:41 -0600

    Remember the staff on BoxingDay

--- customize.sh
+++ customize.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+UMIKAZE_BRANCH="BoxingDay"
+UMIKAZE_REPOSITORY="https://github.com/Wackerbarth/Umikaze"
+REDEEM_BRANCH="b"
+REDEEM_REPOSITORY="https://github.com/Wackerbarth/redeem"
 
 add_custom_account() {
   :
