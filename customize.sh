db3f55955b488203c295aa848d61ba7f08bd9c2b
Richard Wackerbarth <rkw@dataplex.net>
2019-04-03 16:17:43 -0500

    Lee Surrenders - Civil War Ends
    Appomattox Court House, Virginia
    April 9, 1865
    
    General Robert E Lee surrendered the Army of Northern Virginia to General Ulysses S. Grant.

---
+++ customize.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# My personal changes
+# REDEEM_BRANCH="2.2.x"
+# REDEEM_REPOSITORY="Private:Redeem"
+REDEEM_REPOSITORY="https://github.com/Wackerbarth/redeem"
+# TOGGLE_BRANCH="1.3.x"
+# TOGGLE_REPOSITORY="Private:Toggle"
+# TOGGLE_REPOSITORY="https://github.com/Wackerbarth/toggle"
+UMIKAZE_BRANCH="Appomattox"
+# UMIKAZE_REPOSITORY="Private:Umikaze"
+UMIKAZE_REPOSITORY="https://github.com/Wackerbarth/Umikaze"
+
+source ${VERSIONING}/_Version
+# REDEEM_ENVIRONMENT="/usr/local/Redeem-${REDEEM_BRANCH}"
+# TOGGLE_ENVIRONMENT="/usr/local/Toggle-${TOGGLE_BRANCH}"
+
+### This function is run in the chrooted environment
+add_custom_accounts() {
+  :
+}
+### These functions are not run in the chrooted environment
+before_running_in_chroot() {
+  :
+}
+after_running_in_chroot() {
+  :
+}
